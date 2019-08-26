12b6948fbae5338fbb44cb381918959debeab1cc
Student <example@example.com>
2019-08-26 14:04:04 -0700

    added stuff

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 import Navbar from './Navbar.js'
+import MapWithAMarker from './lib/GoogleApi.js'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
 
+      <MapWithAMarker
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJLlQVUA2SO6a9OMFpcrcwv_9c2vH9JJU&v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `700px`, width: '1000px', float: 'right' }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     </div>
   );
 }
